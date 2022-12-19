fb533d15420cec72c39c5ed27ed533893bc28d6a
trista_lea <tristan_lea@outlook.com>
2022-12-18 20:54:50 -0800

    added skeleton

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+var highscores = document.querySelector("#highscores-page");
+
+highscores.addEventListener("click", () => {});
+
+var quesitons = 
+[
+    {
+        //idk how to build one of these
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+]
+
+function startTimer()
+{
+    //on any start button
+}
+
+function getQuestion()
+{
+    //on submit
+}
