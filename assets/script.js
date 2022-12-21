0a1829e736802551baa3ac4d0276789cf8968b70
trista_lea <tristan_lea@outlook.com>
2022-12-21 10:21:24 -0800

    updating code, fleshing out components and trying to get things to show clearly

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,20 @@
+//dom hooks to connect to index.html
 var highscores = document.getElementById("highscores-page");
-console.log("script load");
-highscores.addEventListener("click", startTimer());
-// start.addEventListener("click", () => {startTimer});
-// startTimer();
+var timer = document.getElementById("timer");
+var place1 = document.getElementById("place1");
+var place2 = document.getElementById("place2");
+var place3 = document.getElementById("place3");
+var place4 = document.getElementById("place4");
+var page = 0;
+timer = 100;
+
+// will use later for another html page for highscores
+var scores;
+var homepage = 0; // set page equal to selected page to change
+
+// console.log("script load");
+// highscores.addEventListener("click", startTimer());
+
 var quesitons = 
 [
     {
@@ -27,20 +39,31 @@ var quesitons =
     },
 ]
 
+function start()
+{
+    startTimer;
+    quizStart;
+
+}
+
 function startTimer()
 {
     console.log("start timer ");
     setInterval(timerCallback, 1000);
 }
 
-function getQuestion()
-{
-    //on submit
-}
-timer = 100;
-
 function timerCallback()
 {
     console.log(timer);
     timer--;
+}
+
+function quizStart()
+{
+    
+}
+
+function getQuestion()
+{
+    //on submit
 }
