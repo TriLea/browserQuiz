cd26f209e22c2f36c108c29202604a76de726fa7
trista_lea <tristan_lea@outlook.com>
2023-01-13 17:34:33 -0800

    updating stuff, quick save not finished

--- assets/script.js
+++ assets/script.js
@@ -8,71 +8,69 @@ var question = document.getElementById("Question");
 //dom hooks for choices of answers list, holds 4 radio inputs
 var place1 = document.getElementById("place1");
 var p1Label = document.getElementById("place1Label");
-
 var place2 = document.getElementById("place2");
 var p2Label = document.getElementById("place2Label");
-
 var place3 = document.getElementById("place3");
 var p3Label = document.getElementById("place3Label");
-
 var place4 = document.getElementById("place4");
 var p4Label = document.getElementById("place4Label");
-
 var submit = document.getElementById("submit");
 
 //DOM hooks for highscore final page
 var saveQuestion = document.getElementById("saveQuestion");
-
 var saveYes = document.getElementById("Yes");
-console.log(saveYes);
 var saveNo = document.getElementById("No");
-console.log(saveNo);
-
 var HighscoreDisplay = document.getElementById("Highscores-page");
-
-var time = 200; //in seconds formated by setInterval
-var currentQuestion = 0; //used as a index to interate through list of n number of questions in question array
-var timerObj; //holds return of setInerval to save and stop timer
 
 console.log("script load");
 startButton.addEventListener("click", start); //self explanitory
 submit.addEventListener("click", checkAnswer);
+
+// uses event to listen for click on any child of the parent element, will use this to check if i need to save or not
 document.getElementById("Submit2").addEventListener("click", function (event) {
-    // console.log(event.target);
+
     console.log(this.previousElementSibling.checked);
     console.log(this.previousElementSibling.previousElementSibling.checked);
     saveScore();
 });
 
-var questions =
+var questionsList = //self explanitory
     [
         {
-            title: "what is CSS",
-            choices: ["Something that makes you sick", "Cascading stylesheets", "", "4"],
-            correctAnswer: "CSS"
+            title: "what is CSS?",
+            choices: ["Something that makes you sick", "Cascading stylesheets", "A lowlevel language", "A high level language"],
+            correctAnswer: "Cascading stylesheets"
         },
         {
-            title: "q2",
-            choices: ["a1", "a2", "a3", "a4"],
-            correctAnswer: "a2"
+            title: "What is html?",
+            choices: ["Hyper text markup language", "A thing hackers use", "An IDE", "A low level language"],
+            correctAnswer: "Hyper text markup language"
         },
         {
-            title: "idk3",
-            choices: ["idk3", "1", "2", "3"],
-            correctAnswer: "3"
+            title: "What is javascript?",
+            choices: ["Middle ware", "A video game", "A food", "A scripting language"],
+            correctAnswer: "a scripting language"
         },
         {
-            title: "00",
-            choices: ["01", "02", "03", "04"],
-            correctAnswer: "01"
+            title: "what is a css selector?",
+            choices: ["A way to select html elements", "A data structure", "A empty page", "Hyper text markup language"],
+            correctAnswer: "A way to select html elements"
         }
     ]
 
+//start of logic _____________________________________________________________
+
+//initializes a few things it will need.
+var time = 200; //in seconds formated by setInterval
+var currentQuestion = 0; //used as a index to interate through list of n number of questions in question array
+var timerObj; //holds return of setInerval to save and stop timer
+
 function start() {
-    questionPage.style.display = 'block';
+
+    questionPage.style.display = 'block'; //forget why i need this?
+
     startTimer();
     getQuestion();
-
 }
 
 function startTimer() {
@@ -80,8 +78,8 @@ function startTimer() {
 }
 
 function timerCallback() {
-    if (time <= 0) //ends timer
-    {
+
+    if (time <= 0) { //ends timer and displays highscore page, falls through the decrimentor if timer still has time
         testComplete();
     }
 
@@ -91,18 +89,63 @@ function timerCallback() {
 
 function getQuestion() //displays next question, can do any amount
 {
-    if (!questions[currentQuestion]) {
+    if (!questionsList[currentQuestion]) { // !variable is a way of checking if variable is null or undefined
         return false;
     }
 
-    var currentQuestionLocal = questions[currentQuestion];
-    question.innerHTML = currentQuestionLocal.title;
+    var currentQuestionLocal = questionsList[currentQuestion];
+    question.innerHTML = currentQuestionLocal.title; //what is this for?
+    
     place1Label.innerHTML = currentQuestionLocal.choices[0];
     place2Label.innerHTML = currentQuestionLocal.choices[1];
     place3Label.innerHTML = currentQuestionLocal.choices[2];
     place4Label.innerHTML = currentQuestionLocal.choices[3];
 
     return true;
+}
+
+function checkAnswer() {
+    var userChoice;
+
+    if (place1.checked) {
+        userChoice = place1Label;
+        place1.checked = false; //clean up
+    }
+    else if (place2.checked) {
+        userChoice = place2Label;
+        place2.checked = false; //clean up
+    }
+    else if (place3.checked) {
+        userChoice = place3Label;
+        place3.checked = false; //clean up
+    }
+    else if (place4.checked) {
+        userChoice = place4Label;
+        place4.checked = false; //clean up
+    }
+    else {
+        alert("Please select an answer");
+        time -= 20;
+        timer.innerHTML = time;
+        return;
+    }
+
+    if (userChoice.innerHTML == questionsList[currentQuestion].correctAnswer) {
+        console.log("correct");
+        time += 20;
+        timer.innerHTML = time;
+    }
+    else {
+        console.log("wrong");
+        time -= 20;
+        timer.innerHTML = time;
+    }
+
+    currentQuestion++; //bumps it up when we are done with it
+
+    if (!getQuestion()) {
+        testComplete();
+    }
 }
 
 function saveScore() // get this from time
@@ -192,50 +235,6 @@ function saveScore() // get this from time
     // //saves to local storage
     // //update highscores??
     // //displays play again button
-}
-
-function checkAnswer() {
-    var userChoice;
-
-    if (place1.checked) {
-        userChoice = place1Label;
-        place1.checked = false; //clean up
-    }
-    else if (place2.checked) {
-        userChoice = place2Label;
-        place2.checked = false; //clean up
-    }
-    else if (place3.checked) {
-        userChoice = place3Label;
-        place3.checked = false; //clean up
-    }
-    else if (place4.checked) {
-        userChoice = place4Label;
-        place4.checked = false; //clean up
-    }
-    else {
-        alert("Please select an answer");
-        time -= 20;
-        timer.innerHTML = time;
-        return;
-    }
-
-    if (userChoice.innerHTML == questions[currentQuestion].correctAnswer) {
-        console.log("correct");
-        time += 20;
-        timer.innerHTML = time;
-    }
-    else {
-        console.log("wrong");
-        time -= 20;
-        timer.innerHTML = time;
-    }
-
-    currentQuestion++; //bumps it up when we are done with it
-
-    if (!getQuestion()) {
-        testComplete();
-    }
 }
 
 // function saveYesOrNo()
